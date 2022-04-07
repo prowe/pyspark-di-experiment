23ed8992c7b73829165defcfdc34d51527f702c1
Paul Rowe <prowe@sourceallies.com>
2022-04-06 20:34:08 -0500

    maybe just invoke it?

--- src/build_fruits_job.py
+++ src/build_fruits_job.py
@@ -1,17 +1,31 @@
-from framework import run_with_local_spark
+from framework import run_with_local_spark, data_source, data_sink, data_calculator
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder.getOrCreate()
+
+@data_source
 def load_colors(): 
-    pass
+    return 'color'
 
 
+@data_source
 def load_foods():
+    return 'food'
+
+
+@data_sink
+def save_foods(foods):
+    print('Saving', foods)
     pass
 
 
+@data_calculator(sink = save_foods)
 def create_foods_dataset():
-    pass
+    foods = load_foods()
+    colors = load_colors()
+    return foods + colors
 
 
 if __name__ == '__main__':
     print('In main')
-    run_with_local_spark(__name__)
+    create_foods_dataset()
--- src/framework.py
+++ src/framework.py
@@ -1,6 +1,53 @@
+from ast import Call
 import sys
+import inspect
+from textwrap import wrap
+from types import ModuleType
+from typing import Callable, List
+
+
+def data_source(source_func: Callable) -> Callable:
+    return source_func
+
+
+def data_sink(sink_func: Callable) -> Callable:
+    return sink_func
+
+
+# Decorator args are addtional arguments here
+def data_calculator(*args, **kwargs) -> Callable:
+    sink: Callable = kwargs['sink']
+    def wrap_calculator_func(calc_func):
+        def wrapped_calculator(*args, **kwargs):
+            result = calc_func(*args, **kwargs)
+            print('Got result')
+            sink(result)
+            return result
+    
+        return wrapped_calculator
+
+    return wrap_calculator_func
+
+
+def _is_calculator_function(func: object) -> bool:
+    if not inspect.isfunction(func):
+        return False
+
+    if func.__name__ == 'create_foods_dataset':
+        print(func)
+
+    return True
+
+
+def _find_output_functions(job_module: ModuleType) -> List[Callable]:
+    return [f for (_, f) in inspect.getmembers(job_module) if _is_calculator_function(f)]
+
 
 def run_with_local_spark(module_name: str):
     print('Running with local spark', module_name)
     job_module = sys.modules[module_name]
     print('module', job_module)
+
+    output_functions = _find_output_functions(job_module)
+    for out_func in output_functions:
+        print(out_func)
